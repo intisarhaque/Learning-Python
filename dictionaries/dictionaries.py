2fc4c42ef4d70a1884848ab22b69eec4ee2e8431
intisar <int.haque@hotmail.com>
2019-06-09 01:07:54 +0100

    dict done

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -1,9 +1,13 @@
-locations = {0: "GameOver",
-             1: "Horns Road",
-             2: "Trinity Way",
-             3: "Tesco",
-             4: "High Street",
-             5: "ICHS"}
+
+
+
+
+# locations = {0: "GameOver",
+#              1: "Horns Road",
+#              2: "Trinity Way",
+#              3: "Tesco",
+#              4: "High Street",
+#              5: "ICHS"}
 
 #each direction gives us the key of the destination in the locations directory
 # exits = [{"Q":0},
@@ -13,45 +17,45 @@ locations = {0: "GameOver",
 #          {"N":5, "W":2, "Q":0},
 #          {"S":4, "Q":0}]
 
-exits = {0: {"Q":0},
-         1: {"N":2, "Q":0},
-         2: {"W":3, "E":4, "S":1, "Q":0},
-         3: {"E":2, "Q":0},
-         4: {"N":5, "W":2, "Q":0},
-         5: {"S":4, "Q":0}}
+# exits = {0: {"Q":0},
+#          1: {"N":2, "Q":0},
+#          2: {"W":3, "E":4, "S":1, "Q":0},
+#          3: {"E":2, "Q":0},
+#          4: {"N":5, "W":2, "Q":0},
+#          5: {"S":4, "Q":0}}
+#
+# alias = {"Q":"Q", "QUIT":"Q",
+#          "N":"N", "UP":"N", "NORTH":"N",
+#          "S": "S", "DOWN": "S", "SOUTH": "S",
+#          "E": "E", "RIGHT": "E", "EAST": "E",
+#          "W": "W", "LEFT": "W", "WEST": "W"}
 
-alias = {"Q":"Q", "QUIT":"Q",
-         "N":"N", "UP":"N", "NORTH":"N",
-         "S": "S", "DOWN": "S", "SOUTH": "S",
-         "E": "E", "RIGHT": "E", "EAST": "E",
-         "W": "W", "LEFT": "W", "WEST": "W"}
 
-
-loc = 1
-while True:
-    availableExits = ""
-    print("You are at " + locations[loc] + "\nDirections available:")
-    for direction in exits[loc].keys():
-        print(direction,  end=' ')
-        availableExits += direction
-
-    theirdirection = input("Where do you want to go?\t").upper()
-    word = theirdirection.split()
-    for w in word:
-        error = True
-        if w in alias:
-            direction = alias[w]
-            print(direction)
-            error = False
-    if error == True:
-        print("Invalid type")
-        continue
-    if direction == 'Q':
-        break
-    if direction in availableExits:
-        loc = exits[loc][direction]
-    else:
-        print("Unavailable route")
+# loc = 1
+# while True:
+#     availableExits = ""
+#     print("You are at " + locations[loc] + "\nDirections available:")
+#     for direction in exits[loc].keys():
+#         print(direction,  end=' ')
+#         availableExits += direction
+#
+#     theirdirection = input("Where do you want to go?\t").upper()
+#     word = theirdirection.split()
+#     for w in word:
+#         error = True
+#         if w in alias:
+#             direction = alias[w]
+#             print(direction)
+#             error = False
+#     if error == True:
+#         print("Invalid type")
+#         continue
+#     if direction == 'Q':
+#         break
+#     if direction in availableExits:
+#         loc = exits[loc][direction]
+#     else:
+#         print("Unavailable route")
 
 
 
